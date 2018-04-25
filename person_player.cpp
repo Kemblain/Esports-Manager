0341bc2adc2a57573cc78d60085f2c12ee5045bc
Kem <trbkem@gmail.com>
2018-04-24 21:59:03 -0400

    Add files via upload
    first commit of basic files

---
+++ person_player.cpp
@@ -0,0 +1,15 @@
+#include "person_player.h"
+#include <string>
+#include <iostream>
+#include <random>
+
+
+person_player::person_player()
+{
+    //ctor
+}
+
+person_player::~person_player()
+{
+    //dtor
+}
