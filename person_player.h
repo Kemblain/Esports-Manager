36cd399ec0ad8063c34a9dba4f248c9d142e4235
Kem <trbkem@gmail.com>
2018-04-27 21:27:56 -0400

    Add files via upload
    uploading beginning of nationality class and structure before I go to sleep

--- person_player.h
+++ person_player.h
@@ -8,22 +8,28 @@ class person_player
         person_player();
         virtual ~person_player();
         std::string to_string();
+        int getAge(unsigned short dateOfBirth);
 
     protected:
 
     private:
         unsigned short attributeOne, attributeTwo, attributeThree, attributeFour, attributeFive, attributeSix, attributeSeven, attributeEight, attributeNine, attributeTen,
-                            personID, teamID, gameID, roleID, nationality, currentAbility, potentialAbility, reputation, age, dateOfBirth; //Epoch for age and DOB: January 1 1985 = 0, January 2 1985 = 2 etc.
+                            personID, teamID, gameID, roleID, nationality, currentAbility, potentialAbility, reputation, dateOfBirth; //Epoch for age and DOB: January 1 1985 = 0, January 2 1985 = 2 etc.
         std::string nameFirst, nameLast, nameMiddle, namePseudo, mediaDescription, biography;
         long salary;
+        bool isMale;
 
-        void generateAttribute();
+        void generateAttributes();
         void generateAbilities();
-        void updateAbility();
-        void updateAttributes();
         void generateNationality();
         void generateNames();
+        void generateReputation();
+
+        void updateAbility();
+        void updateAttributes();
         void updateReputation();
+
+        int randConvert(int inputNumber);
 };
 
 #endif // PERSON_PLAYER_H
